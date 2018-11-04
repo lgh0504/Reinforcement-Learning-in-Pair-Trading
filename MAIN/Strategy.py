ef2b6433d79239e49018816ed09cf4336bdd1122
waichungip <39086021+waichungip@users.noreply.github.com>
2018-11-04 21:51:16 +0000

    update strategy with trade method

---
+++ MAIN/Strategy.py
@@ -0,0 +1,155 @@
+from statsmodels.tsa.stattools import coint
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import numpy as np
+
+
+class EGCoint(object):
+
+    def __init__(self, x, y, on, col_name):
+        self.x, self.y, self.time = EGCoint.clean_data(x, y, on, col_name)
+        self.alpha = None
+        self.beta  = None
+        self.resid_mean = None
+        self.resid_std  = None
+        self.cl = None
+
+    @classmethod
+    def clean_data(cls, x, y, on, col_name):
+        x.replace([np.inf, -np.inf], np.nan, inplace=True)
+        y.replace([np.inf, -np.inf], np.nan, inplace=True)
+        merged_df = pd.merge(left=x, right=y, on=on, how='outer')
+        clean_df  = merged_df.loc[merged_df.notnull().all(axis=1), :]
+        return clean_df[col_name + '_x'].values.reshape(-1, 1), \
+               clean_df[col_name + '_y'].values.reshape(-1, 1), \
+               clean_df['date'].values
+
+    def cal_spread(self, x, y):
+        resid      = y - (self.alpha + x * self.beta)
+        norm_resid = (resid - self.resid_mean)/self.resid_std
+        return norm_resid
+
+    def get_sample(self, start, end):
+        assert start < end < len(self.x), 'Error:Invalid Indexing.'
+        x_sample    = self.x[start:end]
+        y_sample    = self.y[start:end]
+        time_sample = self.time[start:end]
+        return x_sample, y_sample, time_sample
+
+    @staticmethod
+    def test_coint(x, y, cl):
+        try:
+            _, p_val, _ = coint(x, y)
+            return p_val if p_val < cl else False
+        except ValueError:
+            print('Exception Encountered: ValueError.')
+            return False
+        except Exception as e:
+            print('Exception Encountered: {}.'.format(e))
+            return False
+
+    def run_ols(self, x, y):
+        reg = LinearRegression().fit(x, y)
+        self.alpha = reg.intercept_
+        self.beta  = reg.coef_[0]
+        resid = y - (self.alpha + self.beta * x)
+        self.resid_mean = resid.mean()
+        self.resid_std  = resid.std()
+
+    def calibrate(self, start, end, cl):
+        self.cl = cl
+        x, y, _ = self.get_sample(start, end)
+        p_val   = self.test_coint(x, y, cl)
+        if p_val:
+            self.run_ols(x, y)
+            spread = self.cal_spread(x, y)
+            return spread, p_val
+        else:
+            return None
+
+    def gen_order(self, start, end, trade_th, stop_loss):
+        assert stop_loss > trade_th, 'Error:Invalid Stop Loss Level.'
+
+        x, y, time = self.get_sample(start, end)
+        spread     = self.cal_spread(x, y)
+        spread_t0  = spread[:-1]
+        spread_t1  = spread[1:]
+        t_t1       = time[1:]
+
+        ind_buy  = np.logical_and(spread_t0 >= -trade_th, spread_t1 <= -trade_th).reshape(-1, )
+        ind_sell = np.logical_and(spread_t0 <= trade_th, spread_t1 >= trade_th).reshape(-1, )
+        ind_stop = np.logical_or(np.logical_and(spread_t0 >= -stop_loss, spread_t1 <= -stop_loss).reshape(-1, ),
+                                 np.logical_and(spread_t0 <= stop_loss, spread_t1 >= stop_loss).reshape(-1, ))
+
+        order = np.array([None] * len(t_t1))
+        order[ind_buy]  = 'Buy'
+        order[ind_sell] = 'Sell'
+        order[ind_stop] = 'Stop'
+        order[-1]       = 'Stop'
+
+        ind_order = order != None
+        time      = t_t1[ind_order]
+        price     = spread_t1[ind_order]
+        order     = order[ind_order]
+
+        return time, price, order
+
+    @staticmethod
+    def gen_trade_record(time, price, order):
+        if len(order) == 0:
+            return None
+
+        n_buy_sell  = sum(order != 'Stop')
+        trade_time  = np.array([None] * n_buy_sell)
+        trade_price = np.array([None] * n_buy_sell)
+        close_time  = np.array([None] * n_buy_sell)
+        close_price = np.array([None] * n_buy_sell)
+        long_short  = np.array([None] * n_buy_sell)
+
+        current_holding = 0
+        j = 0
+
+        for i in range(len(order)):
+            sign_holding = int(np.sign(current_holding))
+            if order[i] == 'Buy':
+                close_pos                    = (sign_holding < 0) * -current_holding
+                close_time[j  - close_pos:j] = time[i]
+                close_price[j - close_pos:j] = price[i][0]
+                trade_time[j]                = time[i]
+                trade_price[j]               = price[i][0]
+                long_short[j]                = 1
+                buy_sell        = close_pos + 1
+                current_holding = current_holding + buy_sell
+                j += 1
+            elif order[i] == 'Sell':
+                close_pos                    = (sign_holding > 0) * -current_holding
+                close_time[j  + close_pos:j] = time[i]
+                close_price[j + close_pos:j] = price[i][0]
+                trade_time[j]                = time[i]
+                trade_price[j]               = price[i][0]
+                long_short[j]                = -1
+                buy_sell        = close_pos - 1
+                current_holding = current_holding + buy_sell
+                j += 1
+            else:
+                close_pos                    = abs(current_holding)
+                close_time[j - close_pos:j]  = time[i]
+                close_price[j - close_pos:j] = price[i][0]
+                current_holding = 0
+
+        profit       = (close_price - trade_price) * long_short
+        trade_record = {'trade_time' : trade_time,
+                        'trade_price': trade_price,
+                        'close_time' : close_time,
+                        'close_price': close_price,
+                        'profit'     : profit}
+
+        return trade_record
+
+    def run_episode(self, start_hist, end_hist, cl, start_forward, end_forward, trade_th, stop_loss):
+        sp = self.calibrate(start_hist, end_hist, cl)
+        if sp is not None:
+            time, price, order = self.gen_order(start_forward, end_forward, trade_th, stop_loss)
+            trade_record = self.gen_trade_record(time, price, order)
+
+
--- TEST/example.py
+++ TEST/example.py
@@ -2,7 +2,7 @@ import datetime
 import time
 import pandas as pd
 from UTIL import aioutil, dateutil, fileutil
-from FACTORY import source
+from MAIN import DataAPI
 
 
 token      = 'YOUR_TOKEN'
@@ -23,7 +23,7 @@ start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
 end_date   = datetime.datetime.strptime(end_date,   '%Y-%m-%d').date()
 
 period   = dateutil.get_dates_weekday(start_date, end_date)
-data_cls = source.get_src_cls('Tiingo')
+data_cls = DataAPI.get_src_cls('Tiingo')
 paras    = [{'url': data_cls.get_url_intraday(ticker, date, freq, token)} for date in period]
 
 
@@ -55,3 +55,154 @@ for data in results:
 output.reset_index(drop=True, inplace=True)
 end = time.time()
 print('Sync processing time: {time}s.'.format(time=end-start))
+
+
+import pandas as pd
+import numpy as np
+from MAIN.Strategy import EGCoint
+import matplotlib.pyplot as plt
+a = pd.read_csv(r'C:\Users\lk258jt\PycharmProjects\Reinforcement-Learning-in-Pair-Trading\FILE\AAPL.csv')
+b = pd.read_csv(r'C:\Users\lk258jt\PycharmProjects\Reinforcement-Learning-in-Pair-Trading\FILE\MSFT.csv')
+o = EGCoint(a,b,'date','close')
+start = 1100
+end = 1200
+sp = o.calibrate(start,end,0.1)
+trade_th = 1
+stop_loss = 2
+time, price, order = o.gen_order(start, end, trade_th, stop_loss)
+trade_record = o.gen_trade_record(time, price, order)
+
+
+x, y, time  = o.get_sample(start, end)
+spread = o.cal_spread(x, y)
+spread_t0 = spread[:-1]
+spread_t1 = spread[1:]
+# x_t1  = x[1:]
+# y_t1  = y[1:]
+t_t1  = time[1:]
+
+
+from timeit import default_timer as timer
+start = timer()
+
+signals = pd.DataFrame()
+signals['time']       = t_t1
+signals['order']      = None
+# signals['x_t1']       = x_t1
+# signals['y_t1']       = y_t1
+signals['spread_t1']  = spread_t1
+
+ind_buy  = np.logical_and(spread_t0 >= -trade_th,  spread_t1 <= -trade_th).reshape(-1,)
+ind_sell = np.logical_and(spread_t0 <=  trade_th,  spread_t1 >=  trade_th).reshape(-1,)
+ind_stop = np.logical_or(np.logical_and(spread_t0 >= -stop_loss, spread_t1 <= -stop_loss).reshape(-1,),
+                         np.logical_and(spread_t0 <=  stop_loss, spread_t1 >=  stop_loss).reshape(-1,))
+signals['order'].loc[ind_buy]  = 'Buy'
+signals['order'].loc[ind_sell] = 'Sell'
+signals['order'].loc[ind_stop] = 'Stop'
+signals['order'].iloc[-1]      = 'Stop'
+signals = signals.loc[signals['order'].notnull(), :]
+
+current_holding = 0
+n_buysell = sum(signals['order'] != 'Stop')
+trade_record = pd.DataFrame(columns=['trade_time', 'trade_price', 'close_time', 'close_price', 'long_short', 'profit'],
+                            index=range(n_buysell))
+j = 0
+
+for i in range(len(signals)):
+    sign_holding = int(np.sign(current_holding))
+    if signals['order'].iloc[i] == 'Buy':
+        close_pos = (sign_holding < 0) * -current_holding
+        trade_record['close_time' ].iloc[j-close_pos:j] = signals['time'].iloc[i]
+        trade_record['close_price'].iloc[j-close_pos:j] = signals['spread_t1'].iloc[i]
+        trade_record['trade_time' ].iloc[j]             = signals['time'].iloc[i]
+        trade_record['trade_price'].iloc[j]             = signals['spread_t1'].iloc[i]
+        trade_record['long_short' ].iloc[j]             = 1
+        buy_sell = close_pos + 1
+        current_holding = current_holding + buy_sell
+        j += 1
+    elif signals['order'].iloc[i] == 'Sell':
+        close_pos = (sign_holding > 0) * -current_holding
+        trade_record['close_time' ].iloc[j+close_pos:j] = signals['time'].iloc[i]
+        trade_record['close_price'].iloc[j+close_pos:j] = signals['spread_t1'].iloc[i]
+        trade_record['trade_time' ].iloc[j]             = signals['time'].iloc[i]
+        trade_record['trade_price'].iloc[j]             = signals['spread_t1'].iloc[i]
+        trade_record['long_short' ].iloc[j]             = -1
+        buy_sell = close_pos - 1
+        current_holding = current_holding + buy_sell
+        j += 1
+    else:
+        close_pos = abs(current_holding)
+        trade_record['close_time' ].iloc[j-close_pos:j] = signals['time'].iloc[i]
+        trade_record['close_price'].iloc[j-close_pos:j] = signals['spread_t1'].iloc[i]
+        current_holding = 0
+
+    trade_record['profit'] = (trade_record['close_price'] - trade_record['trade_price']) * trade_record['long_short']
+
+end = timer()
+print(end-start)
+
+
+
+start = timer()
+
+
+ind_buy  = np.logical_and(spread_t0 >= -trade_th,  spread_t1 <= -trade_th).reshape(-1,)
+ind_sell = np.logical_and(spread_t0 <=  trade_th,  spread_t1 >=  trade_th).reshape(-1,)
+ind_stop = np.logical_or(np.logical_and(spread_t0 >= -stop_loss, spread_t1 <= -stop_loss).reshape(-1,),
+                         np.logical_and(spread_t0 <=  stop_loss, spread_t1 >=  stop_loss).reshape(-1,))
+order = np.array([None] * len(t_t1))
+order[ind_buy]  = 'Buy'
+order[ind_sell] = 'Sell'
+order[ind_stop] = 'Stop'
+order[-1]       = 'Stop'
+time_t1            = t_t1[order != None]
+price           = spread_t1[order != None]
+order           = order[order != None]
+
+current_holding = 0
+n_buy_sell = sum(order != 'Stop')
+trade_time = np.array([None] * n_buy_sell)
+trade_price = np.array([None] * n_buy_sell)
+close_time = np.array([None] * n_buy_sell)
+close_price = np.array([None] * n_buy_sell)
+long_short = np.array([None] * n_buy_sell)
+
+j = 0
+
+for i in range(len(order)):
+    sign_holding = int(np.sign(current_holding))
+    if order[i] == 'Buy':
+        close_pos = (sign_holding < 0) * -current_holding
+        close_time[j-close_pos:j] = time_t1[i]
+        close_price[j-close_pos:j] = price[i][0]
+        trade_time[j]             = time_t1[i]
+        trade_price[j]             = price[i][0]
+        long_short[j]             = 1
+        buy_sell = close_pos + 1
+        current_holding = current_holding + buy_sell
+        j += 1
+    elif order[i] == 'Sell':
+        close_pos = (sign_holding > 0) * -current_holding
+        close_time[j+close_pos:j] = time_t1[i]
+        close_price[j+close_pos:j] = price[i][0]
+        trade_time[j]             = time_t1[i]
+        trade_price[j]             = price[i][0]
+        long_short[j]             = -1
+        buy_sell = close_pos - 1
+        current_holding = current_holding + buy_sell
+        j += 1
+    else:
+        close_pos = abs(current_holding)
+        close_time[j-close_pos:j] = time_t1[i]
+        close_price[j-close_pos:j] = price[i][0]
+        current_holding = 0
+
+profit = (close_price - trade_price) * long_short
+trade_record2 = {'trade_time': trade_time,
+             'trade_price': trade_price,
+             'close_time': close_time,
+             'close_price': close_price,
+             'profit': profit}
+
+end = timer()
+print(end-start)
